f09e265dc422259bd73d6c8d4b4bc0e0f6ed85d3
David Jaramillo <jaramillo.david@outlook.com>
2020-11-09 09:52:11 -0500

    added funtional code

--- main.js
+++ main.js
@@ -10,4 +10,14 @@
           console.log("test failed " +err);
       }
   }
+  //Escribir codigo funcional
+  var romanMatrix = [
+    [5, 'V']
+  ];
+  function romanize(num){
+     if (num >= romanMatrix[0][0]) {
+        return romanMatrix[0][1];
+     }  
+   }
   checkroman();
+
