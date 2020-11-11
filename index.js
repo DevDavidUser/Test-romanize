fe60ff95a7aeff7ad037866b370812b028c04b64
David Jaramillo <jaramillo.david@outlook.com>
2020-11-11 08:02:11 -0500

    added new tests to example

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ function romanize(num){
 */
 checktest(10,'X');
 console.log("test 9 --- el 11 se debe convertir en XI");
-checktest(11,'XI');
+//checktest(19,'XIX');
 /*
 function romanize(num){
     var romanNumber = '';
@@ -196,53 +196,137 @@ function romanize(num){
 }
 */
 // refactorizar codigo
+/*
 function romanize(num){
     var romanNumber = '';
-    
-    function filter(num){
-        if(num<=10){
-            var arraynum = [num];
-            return arraynum;
-        }else{
-            var arraynum = ['10','1'];
-            return arraynum;
+    function getBase(num){
+        if(num <= 3){
+            for(i=0; i<num;i++) {
+                romanNumber += 'I';
+             }
+            return romanNumber
+        }else if(num ===4){
+            return 'IV'
+        }else if(num === 5){
+            return 'V'
+        }else if(num > 5 && num <9){
+            romanNumber = 'V';
+            for(i=0; i<num -5;i++) {
+                romanNumber += 'I';
+             }
+            return romanNumber
+        }else if(num === 9){
+            return 'IX'
+        }else if(num === 10){
+            return 'X'
         }
     }
-    var base = filter(num);
-    console.log(base.length)
-            for(var i=0;i<base.length;i++){
-                console.log(base[i]);
-                switch (base[i]) {
-                    case 1:
-                    case 2:
-                    case 3:
-                        for(i=0; i<base;i++) {
-                            romanNumber += 'I';
-                        }
-                    break;
-                    case 4:
-                        romanNumber = 'IV';
-                    break;
-                    case 5:
-                        romanNumber = 'V';
-                    break;
-                    case 6:
-                    case 7:
-                    case 8:
-                        romanNumber = 'V';
-                        for(i=0; i<num -5;i++) {
-                            romanNumber += 'I';
-                        }
-        
-                    break;
-                    case 9:
-                        romanNumber = 'IX';
-                    break;
-                    case 10:
-                         romanNumber = 'X';
-                    break;
-                }
-                    console.log(romanNumber); 
+    if(num<=10){
+       return getBase(num);
+    }else{
+        const val1 = getBase(num-(num%10));
+        const val2 = getBase(num%10);
+        return romanNumber =val1+val2;
+    }
+}
+*/
+checktest(19,'XIX');
+console.log("test 10 --- el 20 se debe convertir en XX");
+//checktest(21,'XXI');
+/*
+function romanize(num){
+    var romanNumber = '';
+    function getBase(num){
+        if(num <= 3){
+            for(i=0; i<num;i++) {
+                romanNumber += 'I';
+             }
+            return romanNumber
+        }else if(num ===4){
+            return 'IV'
+        }else if(num === 5){
+            return 'V'
+        }else if(num > 5 && num <9){
+            romanNumber = 'V';
+            for(i=0; i<num -5;i++) {
+                romanNumber += 'I';
+             }
+            return romanNumber
+        }else if(num === 9){
+            return 'IX'
+        }else if(num === 10){
+            return 'X'
+        }
+    }
+    if(num<=10){
+       return getBase(num);
+    }else{
+        let mod = num%10;
+        let val1;
+        let val2;
+        if(mod!=0){
+            val1 = getBase(num-(mod));
+            val2 = getBase(mod);
+        }else{
+            mod=10;
+            val1 = getBase(num-(mod));
+            val2 = getBase(mod);
+        }
+        return romanNumber =val1+val2;
+    }
+}
+*/
+checktest(20,'XX');
+console.log("test 11 --- el 21 se debe convertir en XXI");
+checktest(21,'XXI');
+function romanize(num){
+    var romanNumber = '';
+    function getBase(num){
+        if(num <= 3){
+            for(i=0; i<num;i++) {
+                romanNumber += 'I';
+             }
+            return romanNumber
+        }else if(num ===4){
+            return 'IV'
+        }else if(num === 5){
+            return 'V'
+        }else if(num > 5 && num <9){
+            romanNumber = 'V';
+            for(i=0; i<num -5;i++) {
+                romanNumber += 'I';
+             }
+            return romanNumber
+        }else if(num === 9){
+            return 'IX'
+        }else if(num === 10){
+            return 'X'
+        }
+    }
+    if(num<=10){
+       return getBase(num);
+    }else{
+        let mod = num%10;
+        let val1='';
+        let val2='';
+        const base=10;
+        const rep = Math.trunc(num /base);
+        if(mod!=0){
+            console.log(base,rep);
+            for(var i=0;i<rep;i++){
+                val1 = getBase(base);
+                romanNumber += val1;
             }
-    return romanNumber 
+            console.log(romanNumber);
+            val2 = getBase(mod);
+            console.log(val2);
+            return romanNumber += val2;
+        }else{
+            for(var i=0;i<rep;i++){
+                val1 = getBase(base);
+                romanNumber += val1;
+            }
+            return romanNumber
+        }
+    }
 }
